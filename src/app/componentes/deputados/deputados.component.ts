b58158be6e82c2b3cf59541b88c6e2561ebed836
Jose Francisco <chicojfp@gmail.com>
2017-11-07 21:38:04 -0200

    Adicionando rotas e recuperando imagens do site da câmara

--- src/app/componentes/deputados/deputados.component.ts
+++ src/app/componentes/deputados/deputados.component.ts
@@ -20,7 +20,7 @@ export class DeputadosComponent implements OnInit {
   ufs = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT',
     'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR',
     'SC', 'SP', 'SE', 'TO'];
-  ufSelecionada = null;
+  ufSelecionada = 'PE';
 
   constructor(public http: Http) {
     this.recuperarListaDeputados();
